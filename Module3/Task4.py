f51521b20e8817abe85d1b7d49f5e2f4cfbcf2e9
NikaDeg <garelskaiavv1@gmail.com>
2025-08-26 21:22:32 +0300

    new one

---
+++ Module3/Task4.py
@@ -0,0 +1,27 @@
+year = float(input("Enter a year: "))
+if year % 100 == 0 and year % 400 == 0:
+    print(f"{year:.0f} is a leap year.")
+elif year % 4 == 0 and year % 100 != 0:
+    print(f"{year:.0f} is a leap year.")
+else:
+    print(f"{year:.0f} is not a leap year.")
+
+
+
+
+
+
+'''
+just my first attempt without knowing the function %, I left it for memory :)
+
+year = float(input("Enter a year: "))
+result_of_division = year / 4
+division_by_hundred = year / 100
+division_by_fourhundred = year / 400
+if division_by_hundred == int(division_by_hundred) and division_by_fourhundred == int(division_by_fourhundred):
+    print(f"{year:.0f} is a leap year.")
+elif result_of_division == int(result_of_division) and division_by_hundred != int(division_by_hundred):
+    print(f"{year:.0f} is a leap year.")
+else:
+    print(f"{year:.0f} is not a leap year.")
+'''
