723bf483899498624de72a7413310b4258e6ba96
NikaDeg <garelskaiavv1@gmail.com>
2025-09-03 11:00:48 +0300

    new one

--- Module5/lesson.py
+++ Module5/lesson.py
@@ -2,7 +2,7 @@ number = int(input("Please enter an integer number: "))
 while number > 0:
     factorial = 1
     for i in range (1,number+1):
-        factorial = factorial * i
+        factorial = factorial * i  # factorial *= i
     print (f"The factorial of the number: {factorial}")
     number = int(input("Please enter an integer number: "))
 
