90744f16e97160f451e47281debadc97780afb6b
NikaDeg <garelskaiavv1@gmail.com>
2025-09-11 14:32:54 +0300

    new one

--- Module8/Task3.py
+++ Module8/Task3.py
@@ -24,9 +24,9 @@ from geopy.distance import geodesic
 def run_airport_distance():
     city_first = (get_airport_coordinates(code_first))
     city_second = (get_airport_coordinates(code_second))
-    distance = geodesic(city_first, city_second).miles
+    distance = geodesic(city_first, city_second).km
     return distance
-airport_distance = run_airport_distance()*1.6093
+airport_distance = run_airport_distance()
 print(f"Distance between {code_first} and {code_second}: {airport_distance:.2f} kilometers")
 
 # import mysql.connector
