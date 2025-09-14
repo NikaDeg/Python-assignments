56670de25309cc9d9ab8f2b80728b88e485bb791
NikaDeg <garelskaiavv1@gmail.com>
2025-09-14 20:13:32 +0300

    new one

--- Module4/Task2.py
+++ Module4/Task2.py
@@ -3,5 +3,4 @@ while inches >= 0 :
     centimeters = inches*2.54
     print (f"{inches} inches is {centimeters:.2f} centimeters")
     inches = float(input("Enter length in inches (negative value to quit): "))
-if inches < 0 :
-    print ("Program ended.")
+print ("Program ended.")
