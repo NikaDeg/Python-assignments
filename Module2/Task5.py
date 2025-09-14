d423e1eed6519ffbd72292f0af00dc1484aba5eb
NikaDeg <garelskaiavv1@gmail.com>
2025-09-14 11:16:55 +0300

    new one

--- Module2/Task5.py
+++ Module2/Task5.py
@@ -2,6 +2,8 @@ talents = float(input("Enter talents: "))
 pounds = float(input("Enter pounds: "))
 lots = float(input("Enter lots: "))
 total_grams = talents*20*32*13.3 + pounds*32*13.3 + lots*13.3
-kilograms = int(total_grams/1000)
+print(total_grams)
+#kilograms = int(total_grams/1000)
+kilograms = total_grams//1000
 remaining_grams = total_grams - (kilograms*1000)
 print(f"The weight in modern units: \n{kilograms:.0f} kilograms and {remaining_grams:.2f} grams.")
