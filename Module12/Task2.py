361543ce6218330803381079aae9192997dcd12c
NikaDeg <garelskaiavv1@gmail.com>
2025-10-29 08:54:37 +0200

    new

--- Module12/Task2.py
+++ Module12/Task2.py
@@ -2,13 +2,13 @@ import requests
 city = input("Enter municipality name:")
 appcode = "fa30f5dc37496940f69479b66f47783e"
 request = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={appcode}'
-
+lat = ''
+lon = ''
 try:
     response = requests.get(request)
     if response.status_code == 200:
         lat = f"{response.json()[0]['lat']:.2f}"
         lon = f"{response.json()[0]['lon']:.2f}"
-
 except Exception as e:
     print(e)
 
